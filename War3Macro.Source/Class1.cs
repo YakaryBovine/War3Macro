dcc6145d66cc91b95c38a8e941bc78a8b5722169
YakaryBovine <zakaryboven@gmail.com>
2021-11-03 17:33:24 +1300

    initial framework

--- War3Macro.Source/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace War3Macro.Source
-{
-  public class Class1
-  {
-  }
-}
---
+++ War3Macro.Source/ObjectiveEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AzerothWarsCSharp.Source.Libraries
+{
+  public class ObjectiveEventArgs : EventArgs
+  {
+    public Objective Objective { get; }
+
+    public ObjectiveEventArgs(Objective objective)
+    {
+      Objective = objective;
+    }
+  }
+}
